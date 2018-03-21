9c61e141590eb90f9fd9a65769b565fc1fed243c
xiedongming <xie.dongming@kalokeconsulting.com>
2018-03-21 17:08:36 +0800

    Initial Commit

---
+++ CodataDemo_001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodataDemo_001
+//
+//  Created by Admin on 2018/3/21.
+//  Copyright © 2018年 xiedongming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
